252e8268e006eed992434cf6d5b218a7c4325e18
Caleb Hess <redchess64@gmail.com>
2024-09-16 13:28:06 -0400

    god i hope this works

---
+++ home.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "calebh";
+  home.homeDirectory = "/home/calebh";
+
+  home.file.".config/i3/config" = {
+    source = ./i3/config;
+    recursive = true;   # link recursively
+    executable = true;  # make all files executable
+  };
+
+  home.file.".config/polybar/" = {
+    source = ./polybar;
+    recursive = true;
+    executable = true;
+  };
+  
+  home.packages = with pkgs; [
+    neofetch
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Caleb Hess";
+    userEmail = "redchess64@gmail.com";
+  };
+
+  programs.alacritty.enable = true;
+
+  
+  services.polybar = {
+    enable = true;
+    script = ''
+      polybar-msg cmd quit 
+      echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+      polybar -c ~/.config/polybar/config.ini default 2>&1 | tee -a /tmp/polybar1.log & disown
+    '';
+  };
+
+  services.picom = {
+    enable = true;
+    backend = "xrender";
+    vSync = true;
+    settings = {
+      corner-radius = 10;
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
