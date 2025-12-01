6f0e8a1a3cb29a7e66149b8cb024b0c7626dd5ca
Caleb Hess <redchess64@gmail.com>
2025-12-01 18:05:55 -0500

    update to 25.11

--- common/configuration.nix
+++ common/configuration.nix
@@ -43,7 +43,7 @@ in {
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.greetd}/bin/agreety --cmd \"niri-session\"";
+          command = "${pkgs.greetd}/bin/agreety --cmd \"niri-session\"";
         };
       };
     };
