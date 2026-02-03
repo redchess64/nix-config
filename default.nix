c51fce00b379bfaa240674a4bac6364b6e7cae8f
Caleb Hess <redchess64@gmail.com>
2026-02-02 20:32:19 -0500

    format

--- default.nix
+++ default.nix
@@ -3,10 +3,12 @@ let
   pkgs = inputs.pkgs;
   lib = pkgs.lib;
   eval-config = import ((import ./npins).nixpkgs + "/nixos/lib/eval-config.nix");
-  ls = dirs: lib.flatten (
-    map
-      ( dir: (lib.mapAttrsToList (name: _: dir + "/${name}") (builtins.readDir dir)))
-    dirs);
+  ls = dirs:
+    lib.flatten (
+      map
+      (dir: (lib.mapAttrsToList (name: _: dir + "/${name}") (builtins.readDir dir)))
+      dirs
+    );
   mkHost = {modules, ...}:
     eval-config {
       system = null;
